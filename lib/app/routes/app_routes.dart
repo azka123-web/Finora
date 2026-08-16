0fac452ed11626d368426a9c78762066831c556f
Azka Mannan <azkamannan.cs@gmail.com>
2026-08-16 16:18:28 +0500

    Delete lib directory

--- lib/app/routes/app_routes.dart
+++
@@ -1,9 +0,0 @@
-class AppRoutes {
-  static const String splash = '/splash';
-  static const String login = '/login';
-  static const String signup = '/signup';
-  static const String home = '/home';
-  static const String addTransaction = '/add-transaction';
-  static const String editTransaction = '/edit-transaction';
-  static const String transactionHistory = '/transaction-history';
-}
